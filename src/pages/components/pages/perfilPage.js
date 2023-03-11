4a8ef3f04961c677901da56fefc99f24acc6a8c3
MikhaelVinicius <mickaelsoel@gmail.com>
2023-03-11 16:20:29 -0300

    Foto e nome na página usuário add

--- src/pages/components/pages/perfilPage.js
+++ src/pages/components/pages/perfilPage.js
@@ -1,11 +1,20 @@
 import React from 'react'
+import styles from "./Assets/Perfil.module.css"
 
-function perfilPage() {
+function PerfilPage() {
   return (
     <div>
-      
+      <div className={styles.Perfil}>
+        <h1>Perfil de usuário</h1>
+        <img id='img_perfil' src='https://cloudfront-us-east-1.images.arcpublishing.com/estadao/ZFV5J2MOXBPOBPL6DGM2RRRXPU.jpg'/>
+
+        <p> Juliette Freire </p>
+        <button>Itens curtidos</button>
+        <p id='nome'></p>
+        <p id='nome'></p>
+      </div>
     </div>
   )
 }
 
-export default perfilPage
+export default PerfilPage
